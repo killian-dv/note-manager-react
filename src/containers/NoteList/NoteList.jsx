5367a469fddae9ad3a1042cc6132bd95cde8b24b
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-08-04 15:57:48 +0200

    suppression d'une note

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -2,27 +2,36 @@ import { useSelector } from "react-redux";
 import s from "./style.module.css";
 import { TextCard } from "components/TextCard/TextCard";
 import { useNavigate } from "react-router-dom";
+import { NoteApi } from "api/note-api";
+import { useDispatch } from "react-redux";
+import { deleteNote } from "store/note/note-slice";
 
 export function NoteList(props) {
+  const noteList = useSelector((store) => store.NOTE.noteList);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const noteList = useSelector((store) => store.NOTE.noteList);
-    const navigate = useNavigate();
-    return (
-        <div className="row justify-content-center">
-            {noteList.map((note) => {
-                    return (
-                        <div className={s.card_container} key={note.id}>
-                            <TextCard 
-                                title={note.title} 
-                                subtitle={note.subtitle} 
-                                content={note.content}
-                                onClick={() => navigate("/note/" + note.id)}
-                                onClickTrash={() => console.log("Trash clicked")}
-                            />
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+  function deleteNote_(note) {
+    if (window.confirm("Are you sure you wish to delete this item?")) {
+      NoteApi.deleteById(note.id);
+      dispatch(deleteNote(note));
+    }
+  }
+  return (
+    <div className="row justify-content-center">
+      {noteList.map((note) => {
+        return (
+          <div className={s.card_container} key={note.id}>
+            <TextCard
+              title={note.title}
+              subtitle={note.subtitle}
+              content={note.content}
+              onClick={() => navigate("/note/" + note.id)}
+              onClickTrash={() => deleteNote_(note)}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { updateNote } from "store/note/note-slice";
+import { deleteNote } from "store/note/note-slice";
+import { useNavigate } from "react-router-dom";
 
 export function Note(props) {
   const { noteId } = useParams();
@@ -11,13 +13,20 @@ export function Note(props) {
     store.NOTE.noteList.find((note) => note.id === noteId)
   );
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   const [isEditable, setIsEditable] = useState(false);
 
   async function submit(formValues) {
     const updatedNote = await NoteApi.update({ ...formValues, id: note.id });
     dispatch(updateNote(updatedNote));
     setIsEditable(false);
+  }
+  function deleteNote_(note) {
+    if (window.confirm("Are you sure you wish to delete this item?")) {
+      NoteApi.deleteById(note.id);
+      dispatch(deleteNote(note));
+      navigate("/");
+    }
   }
   return (
     <>
@@ -27,7 +36,7 @@ export function Note(props) {
           title={isEditable ? "Edit note" : note.title}
           note={note}
           onClickEdit={() => setIsEditable(!isEditable)}
-          onClickTrash={() => ""}
+          onClickTrash={() => deleteNote_(note)}
           onSubmit={isEditable && submit}
         />
       )}
